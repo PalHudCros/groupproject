41b574cc0a57a9536ea6e3253da1cd9c17750cb2
joseph <josephmpalacio@gmail.com>
2016-10-28 15:35:12 -0500

    nav changes

--- src/container/Nav.js
+++ src/container/Nav.js
@@ -1,21 +1,30 @@
-import React from "react";
-
+import React, { Component } from "react";
+import SuperNav from "./SuperNav";
 
 const styles = {
   nav: {
-    display: "flex"
+    display: "flex",
+    justifyContent: "space-around",
+    width: "100vw"
   }
   , button: {
       color:'red'
   }
 }
 
-export default function Nav(){
-  return (
-    <nav style={styles.nav}>
-      <button style={styles.button}>Home</button>
-      <button style={styles.button}>About</button>
-      <button style={styles.button}>Contact</button>
-    </nav>
-  )
+export default class Nav extends Component{
+  render(){
+    return (
+      <div>
+        <SuperNav></SuperNav>
+        <nav style={ styles.nav }>
+          <button style={ styles.button }>Home</button>
+          <button style={ styles.button }>About</button>
+          <button style={ styles.button }>Contact</button>
+        </nav>
+      </div>
+
+    )
+  }
+
 }
---
+++ src/container/SuperNav.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const styles = {
+  superNav:{
+    display:'flex',
+    justifyContent: "space-between"
+  }
+}
+
+export default class SuperNav extends Component{
+  render(){
+    return(
+    <div style={styles.superNav}>
+      
+    </div>
+    )
+  }
+}
