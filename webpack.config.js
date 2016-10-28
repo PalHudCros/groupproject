71c8f7f55504b52090e01d0748c29add7c73f60e
JJ Crossman <jj.crossman@gmail.com>
2016-10-28 09:27:02 -0500

    edited start script & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
   				test: /\.jsx?$/
   				, exclude: /node_modules/
   				, loader: 'babel'
+          , query: {
+            presets: ["es2015"]
+          }
   			}
   			, {
   				test: /\.css/
