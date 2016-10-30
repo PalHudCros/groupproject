fa9f366fd21c8ad1faa3ba8b7fb323cb17f7f2ef
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-29 22:25:24 -0500

    "Configuring indexRoute for shop"

---
+++ src/shop/component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.scss";
+import React from 'react';
+import {Link} from "react-router";
+
+export default function Home(props){
+  return(
+      <div>
+        <h1>Home</h1>
+        <p>Products here!</p>
+      </div>
+  );
+}
