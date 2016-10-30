a36996649eed8de4661b4f5529e357d34f63d7d0
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-30 17:19:06 -0500

    Removed unique requirement on email property

--- server/features/users/User.js
+++ server/features/users/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const User = new mongoose.Schema({
   facebook_id: {type: String, required: true, unique: true}
   , name: {type: String, required: true}
-  , email: {type: String, unique: true}
+  , email: {type: String}
   , photo: {type: String, default: 'http://www.clker.com/cliparts/m/3/I/C/c/2/grey-silhouette-of-man.svg'}
 });
 
