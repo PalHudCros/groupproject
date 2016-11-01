171635bfa6ed8f39ba4f07945c1945e1e3c08677
joseph <josephmpalacio@gmail.com>
2016-10-31 21:19:01 -0500

    worked on shop view

--- src/shop/component/Shop/Shop.jsx
+++ src/shop/component/Shop/Shop.jsx
@@ -5,7 +5,19 @@ export default function Shop(props) {
 
   return (
       <div>
-        <h1>Shop</h1>
+        <div className="container">
+          <div className="row">
+            
+          </div>
+          <div className="row">
+            <div className="col-sm-3 ctrlPnl-container"  style={{border: 'solid 1px black', backgroundColor:'grey'}}>
+              control panel
+            </div>
+            <div className="col-sm-9 store-container"  style={{border: 'solid 1px black', backgroundColor:'grey'}}>
+              store
+            </div>
+          </div>
+        </div>
       </div>
   )
 }
