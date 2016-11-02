cd687272fd41f3cb01c00fef8e283adfb0ded917
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-02 17:58:18 -0500

    Rewrote query in url

--- server/features/wines/wineCtrl.js
+++ server/features/wines/wineCtrl.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 import config from '../../../config/config';
 import Wine from './Wine';
 
-const baseUrl = 'http://services.wine.com/api/beta/service.svc/json/catalog?offset=0&size=100&apikey=' + config.wineAPI.key + '&filter=';
-const tailUrl = '&sort=popularity%7Cascending&state=TX'
+const baseUrl = 'http://services.wine.com/api/beta/service.svc/json/catalog?offset=0&size=100&apikey=' + config.wineAPI.key + '&sort=popularity%7Cascending&state=TX'
 
 module.exports = {
     getWines(req, res) {
-        axios.get(baseUrl + req.query.id + tailUrl);
+      let query = "";
+      if (req.query) query = '&filter=categories' + req.query.filter; 
+        axios.get(baseUrl + query)
           .then(result => {
             return res.status(200).json(result.data);
           })
