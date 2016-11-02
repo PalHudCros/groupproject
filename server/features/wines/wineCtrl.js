8a5da219454b4894a9bf36bfcf2b5dcb19c99b1f
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-02 17:46:07 -0500

    "Rewrote endpoint to accept query"

--- server/features/wines/wineCtrl.js
+++ server/features/wines/wineCtrl.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 import config from '../../../config/config';
 import Wine from './Wine';
 
-const baseUrl = 'http://services.wine.com/api/beta/service.svc/json/catalog?offset=0&size=100&apikey=' + config.wineAPI.key + '&filter=rating(85%7C100)&sort=popularity%7Cascending&state=TX'
+const baseUrl = 'http://services.wine.com/api/beta/service.svc/json/catalog?offset=0&size=100&apikey=' + config.wineAPI.key + '&filter=';
+const tailUrl = '&sort=popularity%7Cascending&state=TX'
 
 module.exports = {
     getWines(req, res) {
-        axios.get(baseUrl)
+        axios.get(baseUrl + req.query.id + tailUrl);
           .then(result => {
             return res.status(200).json(result.data);
           })
