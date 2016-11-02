64f263647223c40e951c7f3437fa799b4e52bb5d
joseph <josephmpalacio@gmail.com>
2016-11-02 10:36:07 -0500

    wine Iventory Duck

---
+++ src/shop/ducks/wineInventoryDuck.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// Action Type ---------------------------------------------------------------//
+export const WINE_PROCESS = 'WINE_PROCESS';
+export const WINE_SUCCESS = 'WINE_SUCCESS';
+export const WINE_FAILURE = 'WINE_FAILURE';
+
+// Action Creator ------------------------------------------------------------//
+export function reqWineInvt(){
+
+}
+export function recWineInvt(){
+
+}
+export function errWineInvt(){
+
+}
+
+// Async Action --------------------------------------------------------------//
+export function getWineInvt(){
+
+}
+
+// Reducer -------------------------------------------------------------------//
+export fun
