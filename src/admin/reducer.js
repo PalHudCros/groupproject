3afb0920a401798f4ebbf1bf183c564f7d6a1886
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-04 13:27:01 -0500

    Imported inventoryDuck

--- src/admin/reducer.js
+++ src/admin/reducer.js
@@ -5,6 +5,7 @@ import { combineReducers } from "redux";
 import user from "./ducks/userDuck";
 import tabs from "./ducks/tabsDuck";
 import distribution from "./ducks/distributionDuck";
+import inventory from "./ducks/inventoryDuck";
 
 export default combineReducers({
     user
