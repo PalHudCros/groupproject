1ed5da407d083699b75c2eef60104d5eb9ba837e
JJ Crossman <jj.crossman@gmail.com>
2016-11-04 11:16:41 -0500

    planned driver routes and methods

--- server/features/drivers/driverCtrl.js
+++ server/features/drivers/driverCtrl.js
@@ -4,18 +4,16 @@ import Driver from './Driver.js';
 
 module.exports = {
 
-  getDrivers( req, res ) {
-    // GET /api/drivers
-    Driver.find( {}, ( err, drivers ) => {
-      if ( err ) {
-        return res.status( 500 ).json( err );
-      }
-      return res.status( 200 ).json( drivers );
-    } );
+  getOneOrderOnDriver( req, res ) {
+    // GET /api/driver/order/:id
+  }
+
+  , getOrdersOnDriver( req, res ) {
+    // GET /api/driver/order
   }
 
   , getOneDriver( req, res ) {
-    // GET /api/drivers/:id
+    // GET /api/driver/:id
     Driver.findOne( { _id: req.params.id }, ( err, response ) => {
       if ( err ) {
         return res.status( 500 ).json( err );
@@ -24,8 +22,23 @@ module.exports = {
     } );
   }
 
+  , getDrivers( req, res ) {
+    // GET /api/driver
+    Driver.find( {}, ( err, drivers ) => {
+      if ( err ) {
+        return res.status( 500 ).json( err );
+      }
+      return res.status( 200 ).json( drivers );
+    } );
+  }
+
+  , addOrderToDriver( req, res ) {
+    // POST /api/driver/order
+  }
+
+
   , addDriver( req, res ) {
-    // POST /api/drivers
+    // POST /api/driver
     new Driver( req.body ).save( ( err, response ) => {
       if ( err ) {
         return res.status( 500 ).json( err );
@@ -34,14 +47,22 @@ module.exports = {
     } );
   }
 
+  , updateOrderOnDriver( req, res ) {
+    // PUT /api/driver/order/:id
+  }
+
   , updateDriver( req, res ) {
-    // PUT /api/drivers
+    // PUT /api/driver/:id
 
   }
 
+  , removeOrderFromDriver( req, res ) {
+    // DELETE /api/driver/order/:id
+  }
+
   , removeDriver( req, res ) {
-    // DELETE /api/drivers
-    
+    // DELETE /api/driver
+
   }
 
 }
--- server/features/drivers/driverRoutes.js
+++ server/features/drivers/driverRoutes.js
@@ -2,13 +2,22 @@ import driverCtrl from './driverCtrl.js';
 
 module.exports = app => {
 
-  app.route( '/api/drivers/' )
-    .get( driverCtrl.getDrivers )
-    .post( driverCtrl.addDriver )
+  app.route( '/api/driver/order/:id' )
+    .get( driverCtrl.getOneOrderOnDriver )
+    .put( driverCtrl.updateOrderOnDriver )
+    .delete( driverCtrl.removeOrderFromDriver );
+
+  app.route( '/api/driver/order' )
+    .get( driverCtrl.getOrdersOnDriver )
+    .post( driverCtrl.addOrderToDriver );
+
+  app.route( '/api/driver/:id' )
+    .get( driverCtrl.getOneDriver )
     .put( driverCtrl.updateDriver )
     .delete( driverCtrl.removeDriver );
 
-  app.route( '/api/driver/:id' ) {
-    .get( driverCtrl.getOneDriver );
-  }
+  app.route( '/api/driver/' )
+    .get( driverCtrl.getDrivers )
+    .post( driverCtrl.addDriver );
+
 };
