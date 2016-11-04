eb90528ac96c18cb5b503987362238212ffd983c
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-04 11:32:13 -0500

    Added distribution and inventory routes

--- server/features/wines/wineRoutes.js
+++ server/features/wines/wineRoutes.js
@@ -2,6 +2,10 @@ import wineCtrl from './wineCtrl';
 
 module.exports = app => {
     app.get('/api/wines/global', wineCtrl.getWinesFromAPI);
-    app.post('/api/wines/distribution', wineCtrl.addWineToDistribution);
+    app.get('/api/wines/distribution', wineCtrl.getWinesFromDistribution);
+    app.get('/api/wines/inventory', wineCtrl.getWinesFromInventory);
     app.get('/api/wines/distribution/counts', wineCtrl.getCategoryCounts);
+
+    app.post('/api/wines/distribution', wineCtrl.addWineToDistribution);
+    app.post('/api/wines/inventory', wineCtrl.addWineToInventory);
 }
