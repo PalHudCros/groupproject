c42be840afb31d5f8f8ca712c15159f40fe0e655
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-04 12:33:31 -0500

    Updated import to inventoryDuck; removed image manipulation function

--- src/admin/container/DistributorTable/DistributorTable.jsx
+++ src/admin/container/DistributorTable/DistributorTable.jsx
@@ -4,7 +4,7 @@ import {connect} from "react-redux";
 import CircularProgress from 'material-ui/CircularProgress';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import {getWinesFromAPI, addWineToDistribution} from "../../ducks/distributionDuck";
+import {getWinesFromInventory, addWineToInventory} from "../../ducks/inventoryDuck";
 
 
 class DistributorTable extends Component {
@@ -17,14 +17,11 @@ class DistributorTable extends Component {
   }
 
   componentWillMount() {
-    this.props.dispatch(getWinesFromAPI());
+    this.props.dispatch(getWinesFromInventory());
   }
 
   componentWillReceiveProps(props) {
-     const wineList = props.distribution.wines.map(wine => {
-        wine.LabelImage = wine.Labels[0].Url;
-        wine.BottleImage = wine.LabelImage.substring(0, wine.LabelImage.length-5) + "d.jpg";
-        return (
+     const wineList = props.inventory.wines.map(wine => (
           <div key={wine.Id} className="row inventory-row admin">
               <div className="col-xs-4">
                 <img height="150" src={wine.BottleImage} alt=""/>
@@ -39,12 +36,12 @@ class DistributorTable extends Component {
                 </FloatingActionButton>
               </div>
           </div>
-      )});
+      ));
       this.setState({wineList: wineList})
   }
 
   addItemToInventory(wine) {
-    this.props.dispatch(addWineToDistribution(wine));
+    this.props.dispatch(addWineToInventory(wine));
   }
 
   render() {
@@ -66,7 +63,7 @@ class DistributorTable extends Component {
         </div>
         <div className="inventory-rows-wrapper admin">
             {
-              this.props.distribution.status === "Fetching Distribution List"
+              this.props.inventory.status === "Fetching Distribution List"
               ?
               <div className="progress-container">
                 <CircularProgress size={80} thickness={5} />
