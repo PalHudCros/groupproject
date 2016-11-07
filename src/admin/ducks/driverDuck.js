ab827341032efefa605c5f600f2e3abde1d64913
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-07 00:05:56 -0600

    Adding auth to driver and admin routes

--- src/admin/ducks/driverDuck.js
+++ src/admin/ducks/driverDuck.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 // Actions
+const GET_DRIVERS_PROCESS = "driver/GET_DRIVERS_PROCESS";
+const GET_DRIVERS_SUCCESS = "driver/GET_DRIVERS_SUCCESS";
+const GET_DRIVERS_ERROR = "driver/GET_DRIVERS_ERROR";
+
 const CREATE_DRIVER_PROCESS = "driver/CREATE_DRIVER_PROCESS";
 const CREATE_DRIVER_SUCCESS = "driver/CREATE_DRIVER_SUCCESS";
 const CREATE_DRIVER_ERROR = "driver/CREATE_DRIVER_ERROR";
@@ -9,7 +13,110 @@ const DELETE_DRIVER_PROCESS = "driver/DELETE_DRIVER_PROCESS";
 const DELETE_DRIVER_SUCCESS = "driver/DELETE_DRIVER_SUCCESS";
 const DELETE_DRIVER_ERROR = "driver/DELETE_DRIVER_ERROR";
 
-// 
+// Action Creators
+function getDriversProcess() {
+    return {type: GET_DRIVERS_PROCESS}
+}
 
+function getDriversSuccess(driver) {
+    return {type: GET_DRIVERS_SUCCESS, driver}
+}
 
+function getDriversError(err) {
+    retury {type: GET_DRIVER_ERROR, err}
+}
 
+function createDriverProcess() {
+    return {type: GET_DRIVERS_PROCESS}
+}
+
+function createDriverSuccess(driver) {
+    return {type: GET_DRIVERS_SUCCESS, driver}
+}
+
+function createDriverError(err) {
+    retury {type: GET_DRIVER_ERROR, err}
+}
+
+function deleteDriverProcess() {
+    return {type: GET_DRIVERS_PROCESS}
+}
+
+function deleteDriverSuccess(driver) {
+    return {type: GET_DRIVERS_SUCCESS, driver}
+}
+
+function deleteDriverError(err) {
+    retury {type: GET_DRIVER_ERROR, err}
+}
+
+// Async Action Creators 
+export function getDrivers() {
+    return dispatch => {
+       dispatch(getDriversProcess());
+       return axios.get("/api/admin/drivers")
+            .then(results => {
+                dispatch(getDriversSuccess(results.data));
+            })
+            .catch(error => {
+                dispatch(getDriversError(error))
+            }) 
+    }
+}
+
+export function createDriver(email, password) {
+    return dispatch => {
+       dispatch(createDriverProcess());
+       return axios.post("/api/admin/drivers", {})
+            .then(results => {
+                dispatch(createDriverSuccess(results.data));
+            })
+            .catch(error => {
+                dispatch(createDriverError(error))
+            }) 
+    }
+} 
+
+export function deleteDriver(email, password) {
+    return dispatch => {
+       dispatch(deleteDriverProcess());
+       return axios.delete("/api/admin/drivers", {})
+            .then(results => {
+                dispatch(deleteDriverSuccess(results.data));
+            })
+            .catch(error => {
+                dispatch(deleteDriverError(error))
+            }) 
+    }
+} 
+
+// Initial State
+const initialState = {
+    drivers: []
+}
+
+// Reducer
+export default function adminDriver(state = initialState, action) {
+    switch ( action.type ) {
+        case GET_DRIVERS_PROCESS:
+            return Object.assign({}, state, {status: "Fetching Drivers"});
+        case GET_DRIVERS_SUCCESS:
+            return Object.assign({}, state, {drivers: action.drivers}, {status: "Drivers Received!"})
+        case GET_DRIVERS_ERROR:
+            return Object.assign({}, state, {status: "Error", error: action.error});
+        case CREATE_DRIVER_PROCESS:
+            return Object.assign({}, state, {status: "Creating Driver"});
+        case CREATE_DRIVER_SUCCESS:
+            return Object.assign({}, state, {drivers: action.drivers}, {status: "Driver Created!"})
+        case CREATE_DRIVER_ERROR:
+            return Object.assign({}, state, {status: "Error", error: action.error});
+        case DELETE_DRIVER_PROCESS:
+            return Object.assign({}, state, {status: "Deleting Driver"});
+        case DELETE_DRIVER_SUCCESS:
+            return Object.assign({}, state, {drivers: action.drivers}, {status: "Driver Deleted!"})
+        case DELETE_DRIVER_ERROR:
+            return Object.assign({}, state, {status: "Error", error: action.error});
+        default:
+            return state;
+    }
+}
--- src/driver/ducks/userDuck.jsx
+++ src/driver/ducks/userDuck.jsx
@@ -1,27 +1,108 @@
-const LOGIN = "user/LOGIN";
-const LOGOUT = "user/LOGOUT";
+import axios from 'axios';
 
+// Initial state
 const initialState = {
-    username: ""
-    , email: ""
-    , loggedIn: false
+    status: {}
 }
 
-export default function reducer(state = initialState, action) {
-    switch ( action.type ) {
-        case LOGIN:
-            return Object.assign({}, action.user, {loggedIn: true});
-        case LOGOUT:
-            return initialState;
-        default:
-            return state;
+// Actions
+const SHOW_LOCK = 'SHOWLOCK'
+const LOCK_SUCCESS = 'LOCK_SUCCESS'
+const LOCK_ERROR = 'LOCK_ERROR'
+const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+
+// Auth0 Config
+const lock = new Auth0Lock(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, options);
+const options = {
+      allowedConnections: ['Username-Password-Authentication']
+    , theme: {
+        logo: 'https://example.com/assets/logo.png',
+        primaryColor: 'red'
     }
+};
+
+// Action Creators
+// Synchronous Action Creators
+
+function showLock(){
+  return {
+    type:SHOW_LOCK
+  }
 }
 
-export function login(user) {
-    return {type: LOGIN, user};
+function lockSuccess(user){
+  return {
+    type: LOCK_SUCCESS,
+    isAuthenticated: true,
+    user
+  }
 }
 
-export function logout() {
-    return {type: LOGOUT};
+function lockError(err){
+  return {
+    type: LOCK_ERROR,
+    err
+  }
+}
+
+export function logout(){
+  // Clear user token and profile data from localStorage
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('profile');
+  return {
+    type: LOGOUT_SUCCESS
+  }
+}
+
+// Asynchronous Action Creators
+export function login(){
+ return dispatch => {
+   lock.show()
+   return dispatch(showLock)
+ }
+}
+
+export function doAuthentication(){
+  return dispatch => {
+    lock.on('authenticated', function(authResult){
+      lock.getProfile(authResult.idToken, function(err, profile){
+          // Handle auth error
+          if (err) {
+            return dispatch(lockError(err))
+          }
+          // Handle auth success
+          // Set token and profile in local storage
+          localStorage.setItem('profile', JSON.stringify(profile))
+          localStorage.setItem('id_token', authResult.idToken)
+          // Set headers for authentication
+          const config = {
+            headers:{
+            'Accept': 'application/json'
+            , 'Content-Type': 'application/json'
+            , 'Authorization': `Bearer ${authResult.idToken}`
+          }}
+          // Send user profile to database for user
+          return axios.post('/api/user', profile, config)
+            .then(results => {
+              dispatch(lockSuccess(results.data))
+            })
+      })
+    })
+  }
+}
+
+// Reducer
+export default function userReducer(state = initialState, action) {
+  switch(action.type) {
+    case SHOW_LOCK:
+        return Object.assign({}, state, {status: "Logging In"})
+    case LOCK_SUCCESS:
+        return Object.assign({}, state, action.user, {status: "Logged In"})
+    case LOCK_ERROR:
+        return Object.assign({}, state, {status: action.err})
+    case LOGOUT_SUCCESS:
+        return initialState 
+    default:
+        return state
+  }
 }
