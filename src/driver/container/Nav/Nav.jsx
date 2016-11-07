30a4a96df9d621f7b868ab57c450c8a83d64213c
Brian Hudson <brianjasonhudson@gmail.com>
2016-11-07 16:20:30 -0600

    Removed authentication

--- src/driver/container/Nav/Nav.jsx
+++ src/driver/container/Nav/Nav.jsx
@@ -2,10 +2,7 @@ import React, {Component} from 'react';
 import { Link } from 'react-router';
 import { connect } from "react-redux";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AccountCircle from 'material-ui/svg-icons/action/account-circle';
-import IconButton from 'material-ui/IconButton';
-import Avatar from 'material-ui/Avatar';
-import { login, doAuthentication, logout } from "../../ducks/userDuck";
+import {getToken, isTokenExpired} from "../../../utils/jwtHelper";
 
 class Nav extends Component {
   constructor(props) {
@@ -15,17 +12,6 @@ class Nav extends Component {
 
     };
   }
-  componentWillMount() {
-    this.props.dispatch(doAuthentication());
-  }
-
-  handleAuthClick() {
-    if (this.props.user.sub) {
-      this.props.dispatch(logout());
-    } else {
-    this.props.dispatch(login());
-    }
-  }
 
   render() {
 
@@ -33,29 +19,6 @@ class Nav extends Component {
       <nav className="col-xs-12 nav-wrapper driver">
 
           <h1>The Nav Thingy!</h1>
-          <a onClick={this.handleAuthClick.bind(this)}>
-            <span className="UserIcon">
-            {
-              this.props.user.picture
-              ?
-              <Avatar src={this.props.user.picture} size={20} />
-              :
-              <AccountCircle></AccountCircle>
-            }
-              <span className="signIn">
-              {
-                this.props.user.sub
-                  ?
-                "Sign Out"
-                :
-                "Sign In"
-              }
-              </span>
-            </span>
-          </a>
-
-
-
       </nav>
     );
   }
