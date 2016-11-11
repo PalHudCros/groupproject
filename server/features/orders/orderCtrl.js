58b44898eb6c00c1d4c6ac3eea51ab77bb7fe0f9
joseph <josephmpalacio@gmail.com>
2016-11-11 11:27:58 -0600

    great job everyone! checkoutView has addresses

--- server/features/orders/orderCtrl.js
+++ server/features/orders/orderCtrl.js
@@ -5,37 +5,36 @@ import mongoose from 'mongoose'
 
 module.exports = {
   findProductsAndSubtractFromInventory(req, res, next){
-
-    // req.body.products.forEach((ele)=>{
-    //     Wine.findByIdAndUpdate(ele.cart._id, {$inc: {quantity: -ele.quantity}}, (err, quantity)=>{
-    //       if (err) return res.status(500).json(err)
-    //     })
-    // })
+    req.body.products.forEach((ele)=>{
+        Wine.findByIdAndUpdate(ele.cart._id, {$inc: {quantity: -ele.quantity}}, (err, quantity)=>{
+          if (err) return res.status(500).json(err)
+        })
+    })
     next()
   }
   , makeOrder(req, res, next){
-    // User.findOne({sub:req.user.sub}, (err, user) => {
-    //   if (err) return res.status(500).json(err)
-    //   new Order({
-    //     user: user._id,
-    //     products:req.body.products,
-    //     subTotal:req.body.totals.subTotal,
-    //     cartQuantity: req.body.totals.cartQuantity,
-    //     cartTip: req.body.totals.cartTip,
-    //     deliveryFee: req.body.totals.deliveryFee,
-    //     cartTax: req.body.totals.cartTax,
-    //     cartTotal:req.body.totals.cartTotal
-    //   }).save((err, order)=> {
-    //     if (err) return res.status(500).json(err)
-    //   })
-    // })
+    User.findOne({sub:req.user.sub}, (err, user) => {
+      if (err) return res.status(500).json(err)
+      new Order({
+        user: user._id,
+        products:req.body.products,
+        subTotal:req.body.totals.subTotal,
+        cartQuantity: req.body.totals.cartQuantity,
+        cartTip: req.body.totals.cartTip,
+        deliveryFee: req.body.totals.deliveryFee,
+        cartTax: req.body.totals.cartTax,
+        cartTotal:req.body.totals.cartTotal
+      }).save((err, order)=> {
+        if (err) return res.status(500).json(err)
+      })
+    })
     next()
   }
   , deleteCartAndSession(req, res, next){
-    // User.findOne({sub:req.user.sub}, (err, user) => {
-    //   if (err) return res.status(500).json(err)
-    //
-    //})
+    User.findOne({sub:req.user.sub}, (err, user) => {
+      if (err) return res.status(500).json(err)
+    
+    })
     next()
   }
   , getOneOrder(req, res){
