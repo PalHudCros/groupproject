b3d1319e26922feee2018d85d4a04f6aab8aa74b
joseph <josephmpalacio@gmail.com>
2016-11-15 12:47:12 -0600

    fix subdomains, edited .gitignore

--- server/subdomains.js
+++ server/subdomains.js
@@ -8,64 +8,18 @@ module.exports = app => {
   app.use(subdomain('driver', driverRouter))
   app.use(subdomain('admin', adminRouter))
 
-adminRouter.get('/init.js',
-    (req, res)=>{ res.sendFile('init.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/init.js.map',
-    (req, res)=>{ res.sendFile('init.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/admin.js',
-    (req, res)=>{ res.sendFile('admin.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/admin.js.map',
-    (req, res)=>{ res.sendFile('admin.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
+adminRouter.use(express.static(`${__dirname}/..dist/admin`))
   .get('*',
   (req, res)=>{ res.sendFile('index.html', {root:`${__dirname}/../dist/admin`}) }
 );
 
-driverRouter.get('/init.js',
-    (req, res)=>{ res.sendFile('init.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/init.js.map',
-    (req, res)=>{ res.sendFile('init.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/driver.js',
-    (req, res)=>{ res.sendFile('driver.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/driver.js.map',
-    (req, res)=>{ res.sendFile('driver.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
+driverRouter.use(express.static(`${__dirname}/../dist/drivers`))
   .get('*',
     (req, res)=>{ res.sendFile('index.html', {root:`${__dirname}/../dist/driver`}) }
   );
 
-app.get('/*/init.js',
-    (req, res)=>{ res.sendFile('init.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/*/init.js.map',
-    (req, res)=>{ res.sendFile('init.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/*/shop.js',
-    (req, res)=>{ res.sendFile('shop.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/*/shop.js.map',
-    (req, res)=>{ res.sendFile('shop.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/init.js',
-    (req, res)=>{ res.sendFile('init.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/init.js.map',
-    (req, res)=>{ res.sendFile('init.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/shop.js',
-    (req, res)=>{ res.sendFile('shop.js', {root:`${__dirname}/../dist/chunks`}) }
-  )
-  .get('/shop.js.map',
-    (req, res)=>{ res.sendFile('shop.js.map', {root:`${__dirname}/../dist/chunks`}) }
-  )
+app.use(express.static(`${__dirname}/../dist/shop`))
   .get('*',
-      (req, res)=>{ res.sendFile('index.html', {root:`${__dirname}/../dist/shop`}) }
+    (req, res)=>{ res.sendFile('index.html', {root:`${__dirname}/../dist/shop`}) }
   );
-
 }
